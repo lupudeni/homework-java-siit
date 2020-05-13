a03fd08d2bf0c44650fbbc2f842d8d82ee30c916
lupudeni <denisa.s.lupu@gmail.com>
2020-05-13 13:58:26 +0300

    Homework 11 - Balanced Brackets problem (with tests)

--- src/main/java/com/homework/week11/Bracket.java
+++ src/main/java/com/homework/week11/Bracket.java
@@ -6,19 +6,21 @@ import java.util.stream.Collectors;
 public class Bracket {
 
     public boolean isBalanced(String s) {
-        if(s == null) {
+        if (s == null) {
             return false;
         }
         Map<String, String> bracketsMap = Map.of("}", "{", "]", "[", ")", "(");
         Stack<String> stack = new Stack<>();
         List<String> brackets = Arrays.stream(s.split("")).collect(Collectors.toList());
-        for(String bracket : brackets) {
+        for (String bracket : brackets) {
             if (bracketsMap.containsValue(bracket)) {
                 stack.push(bracket);
             } else if (bracketsMap.containsKey(bracket)) {
                 String lastBracket = stack.peek();
                 if (bracketsMap.get(bracket).equals(lastBracket)) {
                     stack.pop();
+                } else {
+                    return false;
                 }
             } else {
                 return false;
